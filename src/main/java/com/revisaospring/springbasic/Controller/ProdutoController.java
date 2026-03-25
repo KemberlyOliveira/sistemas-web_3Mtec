6b96ead7859299589487279b2f04a8a6b9fd4127
kim <oliveirakemberly3@gmail.com>
2026-03-25 14:57:09 -0300

    editar

--- src/main/java/com/revisaospring/springbasic/Controller/ProdutoController.java
+++ src/main/java/com/revisaospring/springbasic/Controller/ProdutoController.java
@@ -9,7 +9,10 @@ import com.revisaospring.springbasic.Entity.Produto;
 import com.revisaospring.springbasic.Service.ProdutoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 @RequestMapping("/produtoCTR")
@@ -35,5 +38,20 @@ public class ProdutoController {
         produtoService.cadastraProduto(oProduto);        
         return "redirect:/produtoCTR/listarProdutos" ;
     }
+
+    @GetMapping("/formAlterar/{id}")
+   public String telaAlterarProduto(@PathVariable Long id, Model oModel){
+         Produto oProduto = produtoService.buscarPorId(id).orElseThrow(() -> new IllegalArgumentException("Produto não encontrado"));
+         oModel.addAttribute("produtoEditar", oProduto);
+                  return "cadastrarProduto";
+   }
+
+   @PostMapping("/alterarProduto/{id}")
+   public String alterarProduto(@PathVariable Long id, @ModelAttribute Produto oProduto) {
+       
+    produtoService.alterarProduto(oProduto, id);
+       return "redirect:/produtoCTR/listarProdutos";
+   }
+   
 
 }
